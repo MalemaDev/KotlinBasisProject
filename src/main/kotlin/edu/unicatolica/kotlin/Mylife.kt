3a35a7a71cf52471c1b66f20e9446dbc2be6b480
MalemaDev <lpeepp030@gmail.com>
2025-08-20 19:39:20 -0500

    Actualizando la tarea de la clase

--- src/main/kotlin/edu/unicatolica/kotlin/Mylife.kt
+++
@@ -1,7 +0,0 @@
-val nombre: Stirng = "Miguel"
-val apellido: String = "Lema"
-val carrera: String = "Desarrollo de software"
-val expectativa: String = "Mi expectativa referente a la carrera es trabajar en el area de BACKEND ya que es la que mas me llama la atencion"
-
-val mensaje = "Hola, mi nombre es $nombre,$apellido, y me encuentro en la carrera $carrera, en la universidad CATOLICA y mi $expectativa"
-println(mensaje)
---
+++ src/main/kotlin/edu/unicatolica/kotlin/mySelf.kt
@@ -0,0 +1,11 @@
+package edu.unicatolica.kotlin
+
+fun main () {
+    val nombre = "Miguel"
+    val apellido = "Lema"
+    val carrera = "Desarrollo de software"
+    val expectativa = "Mi expectativa de la carrera es podeerme dedicar al desarrollo BACKEND ya que es ela ra que mas me llama la atencion"
+
+    println("Mi nombre es $nombre, $apellido y me encuentro estudiando la carrera $carrera en la universidad catolica y mi expectativa de la clase es $expectativa")
+
+}
